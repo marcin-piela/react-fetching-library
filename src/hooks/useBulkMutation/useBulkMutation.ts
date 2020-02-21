5a7acf62358fd0dda7e20c8596ad29834b2d65ee
Josejulio Martínez <jmartine@redhat.com>
2020-02-21 16:49:07 -0600

    Fixes build error when trying to use the spread array operator

--- src/hooks/useBulkMutation/useBulkMutation.ts
+++ src/hooks/useBulkMutation/useBulkMutation.ts
@@ -42,7 +42,7 @@ export const useBulkMutation = <T = any, R = {}, S = any>(
   const handleQuery = useCallback(
     async (actionParams: S[]) => {
       if (!isMounted.current) {
-        return [...Array(actionParams.length)].map(() => ({
+        return Array.from(Array(10).keys()).map(() => ({
           error: false,
         }));
       }
