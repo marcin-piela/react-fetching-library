8bb582f033a24c8decaeb43c663d69a5d7eb5da4
Marcin Piela <marcin.piela@thesoftwarehouse.pl>
2020-02-24 19:28:44 +0100

    fix client context query TS type

--- src/context/clientContext/clientContext.types.ts
+++ src/context/clientContext/clientContext.types.ts
@@ -2,7 +2,7 @@ import { Cache } from '../../cache/cache.types';
 import { Action, QueryResponse, SuspenseCacheItem } from '../../client/client.types';
 
 export type ClientContextType = {
-  query: <T>(actionInit: Action<any>, skipCache?: boolean) => Promise<QueryResponse<T>>;
+  query: <T = any, R = any>(actionInit: Action<T, R>, skipCache?: boolean) => Promise<QueryResponse<T>>;
   cache?: Cache<QueryResponse>;
   suspenseCache: Cache<SuspenseCacheItem>;
 };
